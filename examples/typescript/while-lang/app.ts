5fbcde2156666946f53af5601853b3cfd4663f30
Matts966 <Matts966@users.noreply.github.com>
2020-02-10 16:54:06 +0900

    Update sample of while language to use more detailed syntaxes

--- examples/typescript/while-lang/app.ts
+++ examples/typescript/while-lang/app.ts
@@ -1,8 +1,13 @@
 import * as While from "./WhileLanguage"
 
 While.begin()
-    .var("ok")
-    .assign()
-    .num(100)
+    .while_().true_().do_()
+        .if_().num(100).eq().num(150).minus().num(50).then_()
+            .while_().true_().do_()
+                .skip()
+        .else_()
+            .skip()
+    .andThen()
+        .skip()
     .end()
     .accept()
